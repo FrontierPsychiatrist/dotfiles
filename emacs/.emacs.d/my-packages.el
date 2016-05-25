c630694515762f02a6b9b948c74a8e2fda33f94f
moritz.schulze <moritz.schulze@techdev.de>
2016-05-25 16:31:25 +0200

    Added ensime and evil to the packages

--- emacs/.emacs.d/my-packages.el
+++ emacs/.emacs.d/my-packages.el
@@ -7,7 +7,7 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
-(defvar my-packages '(org company elm-mode seti-theme magit helm))
+(defvar my-packages '(org company elm-mode seti-theme magit helm ensime evil))
 
 (dolist (package my-packages)
   (unless (package-installed-p package)
