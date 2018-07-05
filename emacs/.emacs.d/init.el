b7f327d8ca3c1aa7c2948bad0b1663d95b7735be
Moritz do Rio Schulze <moritz.schulze@techdev.de>
2018-07-05 15:54:14 +0200

    Adds dockerfile-mode

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -79,6 +79,9 @@
   :config
   (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
 
+(use-package dockerfile-mode
+  :ensure t)
+
 ; PATH env variable needed by elm-mode to find elm-make and node
 (setenv "PATH" (concat (getenv "PATH") ":/home/moritz/.npm-packages/bin:/home/moritz/bin"))
 (setq exec-path (append exec-path  '("/home/moritz/bin" "/home/moritz/.npm-packages/bin")))
@@ -138,7 +141,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(js2-mode yaml-mode git-gutter neotree evil helm company doom-themes projectile magit use-package)))
+   '(dockerfile-mode js2-mode yaml-mode git-gutter neotree evil helm company doom-themes projectile magit use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
